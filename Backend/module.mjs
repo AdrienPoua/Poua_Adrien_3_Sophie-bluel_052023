bce04b7087f1bc242beae8d166f404a32f0df6fc
zartax95 <adrienvilb@msn.com>
2023-05-20 13:10:03 +0200

    scss niquel

--- Backend/module.mjs
+++ Backend/module.mjs
@@ -10,7 +10,7 @@ export async function fetchData(data1, data2) {
   }
 }
 export function makeGallery(array) {
-  const gallery = document.querySelector('.gallery')
+  const gallery = document.querySelector('.portfolio__gallery')
   for (let i = 0; i < array.length; i++) {
     const figure = document.createElement('figure');
     const img = document.createElement('img');
@@ -22,24 +22,22 @@ export function makeGallery(array) {
     figcaption.innerText = array[i].title;
   }
 }
-
 export function filter(array) {
   let buttons = document.querySelectorAll('[data-id]');
-  let gallery = document.querySelector('.gallery');
-
+  let gallery = document.querySelector('.portfolio__gallery');
   buttons.forEach((button) => {
     button.addEventListener('click', (event) => {
       let newArray = array.filter((item) => item.categoryId == button.dataset.id);
       if (newArray.length !== array.length) {
         gallery.innerHTML = '';
-        buttons.forEach((btn) => btn.classList.remove('btn--active'));
+        buttons.forEach((btn) =>
+          btn.classList.remove('btn--active'));
         button.classList.add('btn--active');
         makeGallery(button.dataset.id != 0 ? newArray : array);
       }
     });
   });
 };
-
 async function getPromise() {
   let inputEmail = document.querySelector('#input__email').value;
   let inputPassword = document.querySelector('#input__password').value;
@@ -64,8 +62,8 @@ async function getPromise() {
 export async function logIn() {
   let indexUrl = './index.html';
   let errorLog = document.querySelector('.log__error');
-  let logContainer = document.querySelector('.log__container');
-  logContainer.addEventListener('submit', async (e) => {
+  let log = document.querySelector('.log');
+  log.addEventListener('submit', async (e) => {
     e.preventDefault();
     let promise = await getPromise();
     if ('userId' in promise) {
@@ -77,12 +75,36 @@ export async function logIn() {
     }
   });
 }
-
 export function adminMode() {
   let token = sessionStorage.getItem('token');
   if (token != null) {
-    let admin__bar = document.querySelector('.admin__bar');
-    admin__bar.classList.add('visible');
+    visibility();
+    adminListener()
+
   }
+}
+
+function visibility() {
+  let hidden = document.querySelectorAll('.hidden');
+  let logout = document.querySelector('.nav__login');
+  let header = document.querySelector('header')
+  header.style.marginTop = '6em';
+  let btnGallery = document.querySelector('.portfolio ul')
+  logout.innerText = 'logout';
+  btnGallery.classList.add('hidden')
+  hidden.forEach(element => element.classList.toggle('hidden'));
 
 }
+
+function adminListener() {
+  let logout = document.querySelector('.nav__login');
+  let btnModifier = document.querySelectorAll('span .hidden')
+  let myModal = document.querySelector('#myModal')
+  let myModalCloseBtn = document.querySelector('#modal__close')
+  logout.addEventListener('click', () => sessionStorage.clear());
+  btnModifier.addEventListener('click', () => myModal.show())
+  myModalCloseBtn.addEventListener('click', () => myModal.close())
+  }
+
+
+
